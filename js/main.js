bd89d9a78e4e6e847707e003f4cfcaafb626930a
abdulrhman-mustafa <abdulrhmanmustafa00@gmail.com>
2024-12-17 23:46:31 +0200

    Bookstore-Library

---
+++ js/main.js
@@ -0,0 +1,62 @@
+//============================= Website Animation =================================
+
+AOS.init({
+    duration: 2000, // مدة التأثير (بالميلي ثانية)
+    offset: 100,    // المسافة قبل ظهور العنصر
+});
+
+//======================== Swiper Testimonials او اللي بتخلي الاراء تتحرك يمين وشمال=====================
+
+document.addEventListener('DOMContentLoaded', () => {
+    const swiper = new Swiper('.swiper', {
+        // Optional parameters
+        direction: 'horizontal',
+        loop: true,
+        speed: 400,
+        autoplay: true,
+        parallax: true,
+
+
+        navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+        },
+        pagination:{
+            el: '.swiper-pagination',
+        }
+    });
+    })
+
+        //========================= Form And Contact ================================
+       // Login And Register 
+
+        const container = document.getElementById('con');
+        const registerBtn = document.getElementById('register');
+        const loginBtn = document.getElementById('login');
+        
+        registerBtn.addEventListener('click', () => {
+            container.classList.add("active");
+        });
+        
+        loginBtn.addEventListener('click', () => {
+            container.classList.remove("active");
+        });
+        //===================== Btn Scroll الزر بتاع اللي بيطلع الصفحة لفوق ============================
+
+        let btn = document.getElementById("btn-scroll");
+
+        window.onscroll = function (){
+            if(scrollY >= 500) {
+                btn.style.display = 'block';
+            }else 
+            {
+                btn.style.display = 'none';
+            }
+        }
+        btn.onclick = function() {
+            scroll({
+                top: 0,
+            });
+        }
+
+
